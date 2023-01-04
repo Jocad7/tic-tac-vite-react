1ab489a9724e300ab2b0cde3b042203f916d0d6e
jocad <ochlillen@gmail.com>
2023-01-04 19:36:10 -0400

    Current move, highlight square winner added

--- components/Square.jsx
+++ components/Square.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { keyframes } from "styled-components";
 
 const Button = ({ className, onClick, children})=>(
     <button 
@@ -9,9 +10,23 @@ const Button = ({ className, onClick, children})=>(
         {children}
     </button>
 );
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg) scale(1);
+   
+  }
+
+  to {
+    transform: rotate(360deg) scale(.5);
+    
+  }
+`;
+
 const ButtonStyled = styled(Button)`
     width: 100px;
     height: 100px;
+    animation: ${props => props.anim} 1s ease-in-out 2;
+    background:${props=>props.bg};
     padding: 0;
     font-size: 60px;
     font-weight: bold;
@@ -19,11 +34,21 @@ const ButtonStyled = styled(Button)`
 
 
 
-function Square({ value, onSquareClick }){
+function Square({ value, onSquareClick, bgState, index}){
+    let validation = null;
+    let animation = null;
+    if(!!bgState){
+        validation = bgState.some(i=> i=== index) && ' linear-gradient(to right, rgba(255,217,82,1) 0%, rgba(238,225,72,0.8) 40%, rgba(226,231,64,0.8) 69%);';
+        animation = bgState.some(i=> i=== index) && rotate;
+    }
     
     return(
         <>
-            <ButtonStyled className="square" onClick={onSquareClick}>
+            <ButtonStyled 
+            className="square"
+             bg={validation} 
+             anim={animation}
+             onClick={onSquareClick}>
                 {value}
             </ButtonStyled>
         </>
--- src/Board.jsx
+++ src/Board.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
 import { Square } from '../components/Square';
 import styled from 'styled-components';
 import './App.css'
+import { useEffect, useState } from 'react';
 
 const Container = styled.div`
 display: flex;
@@ -11,18 +11,26 @@ const Title = styled.h1`
 `;
 
 function Board({ xIsNext, squares, onPlay, history }) {
+  const [bg, setBg] = useState(null);
+  
   function handleClick(i){
-    
     if(calculateWinner(squares) || squares[i] ){
       return;
     }
     const nextSquares = [...squares];
     nextSquares[i]= xIsNext ? 'x' : 'o';
-    onPlay(nextSquares);
+    onPlay(nextSquares); 
   }
+  /*with this validation i check if all the squares are played, this way i put the message for Draw*/
   const squareLeftValidation = history[history.length - 1].every(item => item !== null)
   const winner = calculateWinner(squares);
-    
+  const squareWinner = !!squareLine(squares);
+  
+  useEffect(()=> {
+    setBg(squareLine(squares))
+  }, [squareWinner]);
+  
+
   let status = winner 
   ? `Winner is: ${winner} 🏆`
   : `Next player is: ${xIsNext ? 'X' : 'O'}`;
@@ -31,26 +39,78 @@ function Board({ xIsNext, squares, onPlay, history }) {
     <>
       <Title>
         {squareLeftValidation ? 'Draw 🎭' : status}
-        </Title>
+      </Title>
       <Container>
-        <Square value={squares[0]} onSquareClick={()=> handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={()=> handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={()=> handleClick(2)}/>
+        <Square
+        index={0}
+        bgState={bg}
+        value={squares[0]}
+        onSquareClick={()=> handleClick(0)}/>
+        <Square
+        index={1}
+        bgState={bg} 
+        value={squares[1]} onSquareClick={()=> handleClick(1)}/>
+        <Square 
+        index={2}
+        bgState={bg}
+        value={squares[2]} onSquareClick={()=> handleClick(2)}/>
       </Container>
       <Container>
-        <Square value={squares[3]} onSquareClick={()=> handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={()=> handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={()=> handleClick(5)}/>
+        <Square
+        index={3}
+        bgState={bg}
+        value={squares[3]} onSquareClick={()=> handleClick(3)}/>
+        <Square
+        index={4}
+        bgState={bg}
+        value={squares[4]} onSquareClick={()=> handleClick(4)}/>
+        <Square
+        index={5}
+        bgState={bg}
+        value={squares[5]} onSquareClick={()=> handleClick(5)}/>
       </Container>
       <Container>
-        <Square value={squares[6]} onSquareClick={()=> handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={()=> handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={()=> handleClick(8)}/>
+        <Square
+        index={6}
+        bgState={bg}
+        value={squares[6]} onSquareClick={()=> handleClick(6)}/>
+        <Square
+        index={7}
+        bgState={bg}
+        value={squares[7]} onSquareClick={()=> handleClick(7)}/>
+        <Square
+        index={8}
+        bgState={bg}
+        value={squares[8]} onSquareClick={()=> handleClick(8)}/>
       </Container>
         
     </>
   )
 }
+
+function squareLine(squares){
+  const winnerSq =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+  for (let i = 0; i < winnerSq.length; i++) {
+    const [a, b, c] = winnerSq[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      
+      return winnerSq[i];
+    }
+  }
+
+  return null;
+}
+
+
 
 function calculateWinner(squares){
   const lines =[
@@ -67,6 +127,7 @@ function calculateWinner(squares){
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      
       return squares[a];
     }
   }
